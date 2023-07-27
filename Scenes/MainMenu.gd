12974a2163d2ee43d2e9ea054c007df029e0583f
Rhenn Handel <rwhandel@gmail.com>
2023-07-27 02:22:14 -0400

    Menu changes, new levels

--- Scenes/MainMenu.gd
+++ Scenes/MainMenu.gd
@@ -2,10 +2,13 @@ class_name MainMenu
 extends BaseScene
 
 func _on_button_pressed():
-	SceneManager.switch_scene("Level0")
+	SceneManager.switch_scene("NewLevel1")
 
 func _on_quit_button_pressed():
 	SceneManager.quit_game()
 
 func _on_tutorial_button_pressed():
 	SceneManager.switch_scene("Tutorial")
+
+func _on_classic_button_pressed():
+	SceneManager.switch_scene("Level0")
